658fad5548cbd9cb2a15bbdc853cbef513d7fd4f
bjrichards <braedenrichards@nevada.unr.edu>
2023-06-01 23:35:26 -0700

    Removed print statement

---
+++ nevada_businesses/export.py
@@ -0,0 +1,13 @@
+import csv
+import dataclasses
+
+from model import business_info
+
+
+def export_to_csv(filename: str, data: list[business_info]):
+    # print(dataclasses.asdict(data[0]))
+    with open(filename, "w", newline="") as f:
+        fields = [field.name for field in dataclasses.fields(business_info)]
+        w = csv.DictWriter(f, fields)
+        w.writeheader()
+        w.writerows([dataclasses.asdict(info) for info in data])
